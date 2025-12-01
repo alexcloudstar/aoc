71638d0bede9207d3352c946ed951e4fe4a1f86a
alexcloudstar <alexcloudstar@gmail.com>
2025-12-01 14:38:29 +0200

    fix: part1.mjs

--- 2025/js/day01/part1.mjs
+++ 2025/js/day01/part1.mjs
@@ -3,35 +3,33 @@ import fs from "fs";
 let pos = 50;
 let clicks = 0;
 
-const contents = fs.readFileSync("part2.txt")
+const contents = fs.readFileSync("part1.txt")
 const lines = contents.toString("utf-8").split("\n");
 
 for (let i = 0; i < lines.length; i++) {
     const direction = lines[i][0];
-    const num = +lines[i].substring(1);
-    const start = pos;
-
-    let hits = 0;
-
-
-    if (direction === "R") {
-        hits = Math.floor((start + num) / 100)
-        pos = (((start + num) % 100) + 100) % 100
-    }
+    const num = +lines[i].substring(1) % 100;
 
     if (direction === "L") {
-        if(start === 0) {
-            hits = Math.floor(num / 100)
-        }
-
-        if (start !== 0 && num >= start) {
-            hits = Math.floor((num - start) / 100) + 1
-        }
-
-        pos = (((start - num) % 100) + 100) % 100
+        pos = pos - num;
     }
 
-    clicks += hits
+    if (direction === "R") {
+        pos = pos + num;
+    }
+
+    if (pos < 0) {
+        pos = pos + 100;
+    }
+
+    if (pos > 99) {
+        pos = pos % 100;
+    }
+
+
+    if (pos === 0) {
+        clicks++;
+    }
 }
 
 console.log("Clicks", clicks);
